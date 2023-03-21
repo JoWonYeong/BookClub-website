ca11b2aa0e6f293c1f520810e4668e7d77634043
JoWonYeong <wswy17@gmail.com>
2023-03-21 12:44:28 +0900

    rentalBook  1차 수정

--- js/rentalBook.js
+++ js/rentalBook.js
@@ -4,38 +4,38 @@ var $month;
 var $date;
 
 $(document).ready(function () {
-    clickEvent();
+  clickEvent();
 });
 
 function clickEvent() {
-    $('#submit1').click(function () {
-        info();
-        $('.info').hide();
-        $('.rent').fadeIn(500);
-    });
-    $('#submit2').click(function () {
-        if (bookCount() > 3) {
-            alert('책은 3권까지 대여 가능합니다.');
-        }
-        else if (bookCount() == 0) {
-            alert('책을 선택해 주세요.');
-        }
-        else {
-            alert(`${$name}(${$number})님 ${$month}월 ${$date}일에 대여 예약되었습니다.`);
-            $('.rent').hide();
-            $('.info').show();
-        }
-    });
+  $('#submit1').click(function () {
+    info();
+    $('.info').hide();
+    $('.rent').fadeIn(500);
+  });
+  $('#submit2').click(function () {
+    if (bookCount() > 3) {
+      alert('책은 3권까지 대여 가능합니다.');
+    } else if (bookCount() == 0) {
+      alert('책을 선택해 주세요.');
+    } else {
+      alert(
+        `${$name}(${$number})님 ${$month}월 ${$date}일에 대여 예약되었습니다.`
+      );
+      $('.rent').hide();
+      $('.info').show();
+    }
+  });
 }
 
 function info() {
-    $name = $('input#setName').val();
-    $number = $('input#setNumber').val();
-    $month = $('.setMonth option:selected').val();
-    $date = $('input.setDate').val();
+  $name = $('input#setName').val();
+  $number = $('input#setNumber').val();
+  $month = $('.select-month option:selected').val();
+  $date = $('input.input-date').val();
 }
 
 function bookCount() {
-    var count = $('input:checkbox[name=book]:checked').length;
-    return count;
+  var count = $('input:checkbox[name=book]:checked').length;
+  return count;
 }
