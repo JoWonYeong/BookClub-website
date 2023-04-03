2dc8af57814969f1ac80bbc6db337b238c696630
JoWonYeong <wswy17@gmail.com>
2023-04-03 11:28:19 +0900

    rentalBook 책 요소 js로 관리

---
+++ js/rentBook_setting.js
@@ -0,0 +1,122 @@
+let 책 = [
+  {
+    id: 0,
+    url: './images/book0.png',
+    title: '46번째 밀실',
+    content:
+      '눈 덮인 고원 별장 성화장. 매년 크리스마스 때가 되면 자신의별장으로 사람들을 초대하는 밀실 추리소설계의 거장 마카베 세이치.그는 올해 역시 크리스마스 파티를 위해 모인 사람들 앞에서 계속 같은 이야기만 쓰는 데 질렸다며 46번째 밀실 작품을 마지막으로 밀실을 졸업하겠다고 선언하고 자리를 뜬다. 그리고 크리스마스 아침. 밀실트릭의 대가는 밀실 상태의 지하 서고 벽난로에 상반신을 들이박고 죽은 처참한 모습으로 발견되는데… 추리소설가 아리스가와 아리스와 임상범죄학자 히무라 히데오가 괴사건의 수수께끼에 도전한다.',
+  },
+  {
+    id: 1,
+    url: './images/book1.png',
+    title: '봉제인형 살인사건',
+    content:
+      '런던의 허름한 아파트에서 신체의 여섯 부위를 꿰매서 이어 붙인 시신 한 구가 발견되었다. 각 신체 부위는 서로 다른 사람의 몸에서 가져온 것이므로, 희생자는 총 여섯 명이다. 사람들은 이를 봉제인형 살인사건이라고 부른다. 그러나 여섯 명의 희생자가 누구인지, 그들의 공통점은 무엇인지에 대해 어떠한 단서도 없어 수사가 미궁에 빠질 무렵, 또 다른 편지 한 통이 울프 형사에게 전달된다.',
+  },
+  {
+    id: 2,
+    url: './images/book2.png',
+    title: '옛날에 내가 죽은 집',
+    content:
+      '7년 전 헤어진 여자친구의 잃어버린 기억을 되찾기 위해 수수께끼 집을 방문, 사건을 추리해나가는 이야기를 담고 있다.',
+  },
+  {
+    id: 3,
+    url: './images/book3.png',
+    title: '해바라기가 피지 않는 여름',
+    content:
+      '미치오는 초등학교 4학년, 부모님과 여동생 미카와 함께 생활한다. 여름 방학을 앞둔 종업식 날, 미치오는 담임인 이와무라 선생님의 심부름으로 결석한 같은 반 친구 S에게 숙제와 유인물을 전해주러 그의 집을 찾아간다. 거기서 미치오는 목을 매고 죽은 S의 시체를 본다. 그런데 소식을 전해들은 이와무라 선생님이 경찰과 함께 달려갔더니, 시신은 어느새 사라지고 없다.',
+  },
+  {
+    id: 4,
+    url: './images/book4.png',
+    title: '공허한 십자가',
+    content:
+      '20년 전, 두 부부가 잠시 잡을 비운 사이 침입한 강도로부터 딸 마나미가 죽고, 더 이상 부부로서의 삶을 살 수 없어 각자의 길로 간다. 나카하라는 5년 전 회사를 관두고 반려동물 장례사로 생활하고 있으며, 그의 전부인 사요코는 최근까지 도벽증 환자들에 대해 취재하고 있었다. 최근 5년 동안 둘 사이엔 큰 왕래가 없었기 때문에 나카하라는 그녀의 장례식에 조문차 다녀간다.',
+  },
+  {
+    id: 5,
+    url: './images/book5.png',
+    title: '메스커레이드 호텔',
+    content:
+      '연쇄살인의 다음 장소로 예고된 매스커레이드 호텔은 최상의 서비스를 상징하는 공간이다. 그런데 이곳에서 벌어지는 의외의 소동이나 손님들은 "최상"이란 수식어에서 한참 멀어 보인다. 심히 정체가 의심스러운 그들의 가면이 벗겨지고 맨얼굴이 드러나는 순간, 그때그때 가면을 바꿔 써야만 하는 얄궂은 운명의 인간 군상을 대면하게 된다.',
+  },
+  {
+    id: 6,
+    url: './images/book6.png',
+    title: '백광',
+    content:
+      '세상이 전부 녹아내릴 듯 뜨겁던 여름날. 어느 가정집 안마당에서 네 살 난 여자아이의 시체가 발견된다. 사망 추정 시간에 호텔에서 불륜을 즐긴 아이의 엄마, 아내의 불륜 사실을 폭로하려던 아이의 아빠, 치과에 예약 진료를 받으러 간 이모, 아이를 데리고 집을 지키던 할아버지, 잠깐 집에 들렀던 이모부, 황급히 집을 뛰쳐나갔던 낯선 남자까지… 여아의 시체를 둘러싸고 평범한 일가족이 각자 감추어오던 충격적인 진실을 고백하며 서로를 살인범으로 지목한다. 한 명, 한 명이 고백할 때마다 범인이 바뀌고 사건이 뒤집히는 믿기 어려운 반전 속에서, 과연 누가 진실을 말하고 누가 거짓을 말하는 걸까? 또 여자아이를 죽인 진짜 범인은 누구일까?',
+  },
+  {
+    id: 7,
+    url: './images/book7.png',
+    title: '용서받지 못한 밤',
+    content:
+      '내 딸이 아내를 죽였다. 엄마를 죽게 한 네살배기 딸, 아빠는 그 죄를 덮었다. 15년 뒤, 비밀을 아는 누군가로부터 전화가 걸려오는데…',
+  },
+  {
+    id: 8,
+    url: './images/book8.png',
+    title: '사과는 떨어지지 않는다',
+    content:
+      '‘엄마의 실종’을 둘러싸고 벌어지는 이야기를 다루고 있는 이번 소설은 주변에 있을 법한 현실적인 캐릭터와 제한된 시간, 현재와 과거를 오가며 맞춰지는 퍼즐식 구성, 각기 다른 진술과 의심으로 증폭되는 쫄깃한 긴장감, 적절하게 깔리는 단서와 마지막 반전까지…',
+  },
+  {
+    id: 9,
+    url: './images/book9.png',
+    title: '조인계획',
+    content:
+      '‘조인鳥人’이라 불리는 스물두 살의 천재 스키점프 선수 니레이 아키라가 합숙 훈련 도중 의문의 죽음을 당한다. 그리고 며칠 뒤 경찰에 익명으로 날아든 한 통의 밀고장. ‘범인은 스키점프팀의 미네기시 코치다. 즉시 체포하시오.’ 미네기시는 혐의를 완강히 부인하지만 살인 용의자로 체포되고, 동료 선수와 스태프 모두는 충격에 빠진다.',
+  },
+  {
+    id: 10,
+    url: './images/book10.png',
+    title: '더러운 손을 거기에 닦지 마',
+    content:
+      '평온하게 여름방학을 마치고 싶은 초등학교 선생, 치매를 앓고 있는 아내에게 상처를 주고 싶지 않은 남편, 오래전 불륜 상대를 되돌아보고 싶지 않은 요리 연구가의 비밀, 죽기 전 남편의 고민을 덜어 주려는 아내, 빛나는 미래를 망치고 싶지 않은 영화감독.',
+  },
+  {
+    id: 11,
+    url: './images/book11.png',
+    title: '전남친의 유언장',
+    content:
+      '거대 제약회사의 후계자인 모리카와 에이지는 「내 전재산을 나를 죽인 범인에게 줄 것!」이라는 기묘한 유언장을 남긴 채 30세의 나이로 사망한다. 학생시절에 그와 3개월간 사귀었던 변호사 켄모치 레이코는 에이지의 친한 친구 하나를 범인으로 내세워 그 대리인 자격으로 범인 선출전에 참여한다. 하지만 유언장 원본이 보관되어 있던 금고를 도난당하고, 에이지의 고문변호사가 의문의 죽음을 당하는데…',
+  },
+];
+
+let rent = document.querySelector('.rent');
+
+책.forEach((a, i) => {
+  // img 요소 만듦 (src에 url, alt에 책제목)
+  let 책_img = document.createElement('img');
+  책_img.setAttribute('src', a.url);
+  책_img.setAttribute('alt', a.title);
+
+  // span.title 요소 만듦 (innerHTML = title)
+  let 책_title = document.createElement('span');
+  책_title.classList.add('title');
+  책_title.innerHTML = a.title;
+
+  // span.content 요소 만듦(innerHTML = content)
+  let 책_content = document.createElement('span');
+  책_content.classList.add('content');
+  책_content.innerHTML = a.content;
+
+  // input.checked 요소 만듦(type='checkbox' name='book' value='book'+id)
+  let 책_checked = document.createElement('input');
+  책_checked.classList.add('checked');
+  책_checked.setAttribute('type', 'checkbox');
+  책_checked.setAttribute('name', 'book');
+  책_checked.setAttribute('value', 'book' + i);
+
+  // article.rent-book에 다 붙이기
+  let article = document.createElement('article');
+  article.classList.add('rent-book');
+  article.appendChild(책_img);
+  article.appendChild(책_title);
+  article.appendChild(책_content);
+  article.appendChild(책_checked);
+
+  rent.appendChild(article);
+});
